6a83fb1f5e609e58984f08b8134186af9fbc9c17
SaiTeja69 <54733232+SaiTeja69@users.noreply.github.com>
2020-05-05 00:24:40 +0530

    Server client interacts now :+1:
    fixed cors error by adding headers.

--- compiler_api/app.py
+++ compiler_api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +9,16 @@ def index():
 def say_hello():
   return 'Hello from Server'
 
-@app.route('/compiler',methods=['POST'])
-def compiler():
-    return "hello"
+@app.route('/api/', methods=["POST"])
+def main_interface():
+    response = request.get_json()
+    x=(response['message'])
+    y=eval(x)
+    response['message']=y
+    return jsonify(response)
+
+@app.after_request
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    return response
--- js/script.js
+++ js/script.js
@@ -1,29 +1,14 @@
-$(document).ready(function() {
-
-	$('form').on('submit', function(event) {
-
-		$.ajax({
-			data : {
-				name : $('#dataField').val(),
-			},
-			type : 'POST',
-			url : 'http://127.0.0.1:5000/compiler'
-		})
-		.done(function(data) {
-
-			if (data.error) {
-				$('#errorAlert').text(data.error).show();
-				$('#successAlert').hide();
-			}
-			else {
-				$('#successAlert').text(data.name).show();
-				$('#errorAlert').hide();
-			}
-
-		});
-
-		event.preventDefault();
-
-	});
-
+$("document").ready(function(){
+    $("#send").click(function(){
+        var message = $("#message").val();
+        $.ajax({
+            url: "http://localhost:5000/api/",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify({"message": message})
+        }).done(function(data) {
+			document.getElementById('compiler').innerHTML=data['message'];
+			console.log(data);
+        });
+    });
 });
