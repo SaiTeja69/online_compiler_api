4ef27f134600b90c1ff9dca3f22eb47faa601db2
SaiTeja69 <54733232+SaiTeja69@users.noreply.github.com>
2020-05-05 21:44:22 +0530

    Revert "Merge branch 'master' of https://github.com/SaiTeja69/online_compiler"
    This reverts commit 9a9bd99b95969fda32debba11bccf3b928b1b242, reversing
    changes made to 720f8882f0cb278ff02888eb4a2905d3f2042bec.

---
+++ README.md
@@ -0,0 +1,2 @@
+# online_compiler
+Online python compiler
---
+++ compiler_api/app.py
@@ -0,0 +1,23 @@
+from flask import Flask,request,jsonify
+import os
+app = Flask(__name__)
+import time
+@app.route('/')
+def index():
+  return 'Server Works!'
+  
+@app.route('/greet')
+def say_hello():
+  return 'Hello from Server'
+
+
+@app.route('/api/', methods=["POST"])
+def main_interface():
+    response = request.get_json()
+    return jsonify(response)
+
+@app.after_request
+def add_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    return response
