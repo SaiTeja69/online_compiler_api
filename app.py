6168e9f920bf22ed0946685ab9edd1d606b64a85
SaiTeja69 <54733232+SaiTeja69@users.noreply.github.com>
2020-05-05 21:44:09 +0530

    Revert "will revert this too"
    This reverts commit 7c399f2d395edfdf8e024566e927de1bfa014965.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask,request,jsonify,render_template
-import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,20 +7,6 @@ def index():
 
 @app.route('/api', methods=["POST"])
 def main_interface():
-    response = request.get_json()
-    print(response)
-    message=response['message']
-    with open('file.py','w') as f:
-      f.write(message)
-      f.close()
-    os.system("python file.py > op.txt")
-    with open('op.txt','r') as f:
-      data=f.read()
-      f.close()
-    response['message']=data
-    return jsonify(response)
-@app.after_request
-def add_headers(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    return response
+    message = request.form['message']
+    result=eval(message)
+    return jsonify({'message': result})
--- file.py
+++
@@ -1,2 +0,0 @@
-print("lmao")
-print('apdi pode')
